29d9c68f8f705d982570dd61a10cc235ca4e7f33
ezgiorman <ezgiorman10@gmail.com>
2024-06-06 21:51:37 +0300

    initial commit.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,151 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Please enter the sizes of the board:  (3-8): ");
+        int size;
+        while (true) {
+            size = scanner.nextInt();
+            if (size >= 3 && size <= 8) {
+                break;
+            } else {
+                System.out.println("Invalid value. Please enter a value between 3 and 8. ");
+            }
+        }
+
+        char[][] board = new char[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = ' ';
+            }
+        }
+
+        char player = 'X';
+        boolean gameWon = false;
+
+        while(!gameWon)
+        {
+            createBoard(board, size);
+            playerMove(board, player, scanner, size);
+            gameWon = checkWinner(board, player, size);
+
+            if(gameWon)
+            {
+                System.out.println("Player " + player + " won the game!");
+                createBoard(board, size);
+                break;
+            }
+            if(isBoardFull(board, size))
+            {
+                System.out.println("No winner!");
+                createBoard(board, size);
+                break;
+            }
+
+            player = (player == 'X') ? 'O' : 'X';
+
+        }
+
+        scanner.close();
+    }
+    public static void createBoard(char[][] board, int size) {
+        System.out.print("  ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < size; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < size; j++) {
+                System.out.print(board[i][j]);
+                if (j < size - 1) System.out.print("|");
+            }
+            System.out.println();
+            if (i < size - 1) {
+                System.out.print("  ");
+                for (int k = 0; k < size; k++) {
+                    System.out.print("-");
+                    if (k < size - 1) System.out.print("+");
+                }
+                System.out.println();
+            }
         }
     }
+
+    public static void playerMove(char[][] board, char player, Scanner scanner, int size) {
+        int row, col;
+        while (true) {
+            System.out.println("Player" + player + ", choose the row and column: ");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+            if (row >= 0 && row < size && col >= 0 && col < size && board[row][col] == ' ') {
+                board[row][col] = player;
+                break;
+            } else {
+                System.out.println("Invalid move, try again. ");
+            }
+
+        }
+    }
+
+    public static boolean checkWinner(char[][] board, char player, int size) {
+        for (int i = 0; i < size; i++) {
+            boolean win = true;
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] != player) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            boolean win = true;
+            for (int j = 0; j < size; j++) {
+                if (board[j][i] != player) {
+                    win = false;
+                    break;
+                }
+            }
+            if (win) {
+                return true;
+            }
+        }
+
+        boolean win = true;
+        for (int i = 0; i < size; i++) {
+            if (board[i][i] != player) {
+                win = false;
+                break;
+            }
+        }
+        if (win) return true;
+
+        win = true;
+        for (int i = 0; i < size; i++) {
+            if (board[i][size - i - 1] != player) {
+                win = false;
+                break;
+            }
+        }
+        return win;
+    }
+
+
+
+    public static boolean isBoardFull(char[][] board, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
